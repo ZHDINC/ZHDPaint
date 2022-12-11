9a5fced09de1eac281b4d5c43373de54da76b86d
ZHDINC <ZHDINC@gmail.com>
2022-12-11 04:02:16 -0500

    Implemented two varieties of brushes to paint with on the screen with a new WM_MOUSEMOVE case and additional buttons needed to support this.

--- ZHDPaint/main.cpp
+++ ZHDPaint/main.cpp
@@ -9,6 +9,8 @@ constexpr int WM_SENDCOLORBRUSH = 0x0803;
 constexpr int BTN_LINE = 1050;
 constexpr int BTN_RECT = 1051;
 constexpr int BTN_ELLIPSE = 1052;
+constexpr int BTN_SQBRUSH = 1055;
+constexpr int BTN_CIRBRUSH = 1056;
 constexpr int BTN_FILLORNOT = 1500;
 constexpr int BTN_DECREASESIZE = 1053;
 constexpr int BTN_INCREASESIZE = 1054;
@@ -79,23 +81,30 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 	HRGN hrgnColorPalette;
 	static HWND fillOrNotCheckBox;
 	static HPEN hpen;
+	static HBRUSH hbrush;
 	static int currentX, currentY;
 	static int currentTool = 0;
 	static bool fillOrNot = false;
 	static int brushSize = 1;
+	static bool drawing = false;
+	RECT drawingBrush;
+	POINT p;
 	switch (message)
 	{
 	case WM_CREATE:
 		
 		hpen = CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
+		hbrush = CreateSolidBrush(RGB(255, 0, 0));
 		hdc = GetDC(hwnd);
 		SelectObject(hdc, hpen);
 		ReleaseDC(hwnd, hdc);
-		hwndColorPalette = CreateWindow(L"Color Palette", nullptr, WS_CHILD | WS_VISIBLE, 500, 0, 200, 100, hwnd, (HMENU)1000, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), nullptr);
+		hwndColorPalette = CreateWindow(L"Color Palette", nullptr, WS_CHILD | WS_VISIBLE, 500, 0, 200, 100, hwnd, (HMENU)1000, (HINSTANCE)GetWindowLong(hwnd, GWLP_HINSTANCE), nullptr);
 		CreateWindow(L"BUTTON", L"Line", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 10, 50, 50, hwnd, (HMENU)BTN_LINE, ((LPCREATESTRUCT)lparam)->hInstance, nullptr);
 		CreateWindow(L"BUTTON", L"Rectangle", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 60, 10, 100, 50, hwnd, (HMENU)BTN_RECT, ((LPCREATESTRUCT)lparam)->hInstance, nullptr);
 		CreateWindow(L"BUTTON", L"Ellipse", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 160, 10, 75, 50, hwnd, (HMENU)BTN_ELLIPSE, ((LPCREATESTRUCT)lparam)->hInstance, nullptr);
 		fillOrNotCheckBox = CreateWindow(L"BUTTON", L"Fill?", WS_CHILD | WS_VISIBLE | BS_CHECKBOX, 250, 10, 50, 50, hwnd, (HMENU)BTN_FILLORNOT, ((LPCREATESTRUCT)lparam)->hInstance, nullptr);
+		CreateWindow(L"Button", L"Square Brush", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 60, 100, 50, hwnd, (HMENU)BTN_SQBRUSH, ((LPCREATESTRUCT)lparam)->hInstance, nullptr);
+		CreateWindow(L"Button", L"Circle Brush", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 110, 60, 100, 50, hwnd, (HMENU)BTN_CIRBRUSH, ((LPCREATESTRUCT)lparam)->hInstance, nullptr);
 		CreateWindow(L"BUTTON", L"-", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 300, 20, 50, 50, hwnd, (HMENU)BTN_DECREASESIZE, ((LPCREATESTRUCT)lparam)->hInstance, nullptr);
 		CreateWindow(L"BUTTON", L"+", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 350, 20, 50, 50, hwnd, (HMENU)BTN_INCREASESIZE, ((LPCREATESTRUCT)lparam)->hInstance, nullptr);
 		
@@ -112,20 +121,47 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 		{
 		case 0:
 			hpen = CreatePen(PS_SOLID, brushSize, RGB(255, 0, 0));
-			
+			hbrush = CreateSolidBrush(RGB(255, 0, 0));
 			break;
 
 		case 1:
 			hpen = CreatePen(PS_SOLID, brushSize, RGB(0, 255, 0));
+			hbrush = CreateSolidBrush(RGB(0, 255, 0));
 			break;
 		case 2:
 			hpen = CreatePen(PS_SOLID, brushSize, RGB(0, 0, 255));
+			hbrush = CreateSolidBrush(RGB(0, 0, 255));
 			break;
 		}
 		return 0;
 	case WM_LBUTTONDOWN:
 		currentX = LOWORD(lparam);
 		currentY = HIWORD(lparam);
+		return 0;
+	case WM_MOUSEMOVE:
+		if (!drawing) return 0;
+		hdc = GetDC(hwnd);
+		if (wparam & MK_LBUTTON)
+		{
+			p.x = LOWORD(lparam);
+			p.y = HIWORD(lparam);
+			switch (currentTool)
+			{
+			case 3:
+				drawingBrush.left = p.x;
+				drawingBrush.top = p.y;
+				drawingBrush.right = p.x + brushSize;
+				drawingBrush.bottom = p.y + brushSize;
+				FillRect(hdc, &drawingBrush, hbrush);
+				break;
+			case 4:
+				SelectObject(hdc, hpen);
+				SelectObject(hdc, hbrush);
+				Ellipse(hdc, p.x, p.y, p.x + brushSize + 1, p.y + brushSize + 1);
+				break;
+			}
+		}
+		ReleaseDC(hwnd, hdc);
 		return 0;
 	case WM_LBUTTONUP:
 		hdc = GetDC(hwnd);
@@ -143,13 +179,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 		{
 		case 0:
 			LineTo(hdc, LOWORD(lparam), HIWORD(lparam));
-			return 0;
+			break;
 		case 1:
 			Rectangle(hdc, currentX, currentY, LOWORD(lparam), HIWORD(lparam));
-			return 0;
+			break;
 		case 2:
 			Ellipse(hdc, currentX, currentY, LOWORD(lparam), HIWORD(lparam));
-			return 0;
+			break;
+		case 3:
+			break;
 		}
 		ReleaseDC(hwnd, hdc);
 		InvalidateRect(hwnd, nullptr, FALSE);
@@ -158,16 +196,19 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 		switch (LOWORD(wparam))
 		{
 		case BTN_LINE:
+			drawing = false;
 			currentTool = 0;
-			return 0;
+			break;
 		case BTN_RECT:
+			drawing = false;
 			currentTool = 1;
-			return 0;
+			break;
 		case BTN_ELLIPSE:
+			drawing = false;
 			currentTool = 2;
-			return 0;
+			break;
 		case BTN_FILLORNOT:
-			if(!Button_GetCheck(fillOrNotCheckBox))
+			if (!Button_GetCheck(fillOrNotCheckBox))
 			{
 				fillOrNot = true;
 				Button_SetCheck(fillOrNotCheckBox, TRUE);
@@ -177,13 +218,23 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 				fillOrNot = false;
 				Button_SetCheck(fillOrNotCheckBox, FALSE);
 			}
-			return 0;
+			break;
+		case BTN_SQBRUSH:
+			drawing = true;
+			currentTool = 3;
+			break;
+		case BTN_CIRBRUSH:
+			drawing = true;
+			currentTool = 4;
+			break;
+
+		case BTN_INCREASESIZE:
+			brushSize++;
+			break;
+		case BTN_DECREASESIZE:
+			brushSize > 1 ? brushSize-- : brushSize = 1;
+			break;
 		}
-	case BTN_INCREASESIZE:
-		brushSize++;
-		return 0;
-	case BTN_DECREASESIZE:
-		brushSize > 1 ? brushSize-- : brushSize = 1;
 		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
